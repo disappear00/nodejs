ebaeca950783b00a1a7bfb721de594ba09a3095f
idontknow <1245785295@qq.com>
2026-06-29 15:31:41 +0800

    解构

--- part1/part1-01/src/App.jsx
+++ part1/part1-01/src/App.jsx
@@ -1,65 +1,25 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
 
-const Part = (props) => {
   return (
     <div>
-      <p>{props.part} {props.exercises}</p>
-    </div>
-  )
-}
-
-// 重构后的 Content 组件，内部渲染三个 Part
-const Content = (props) => {
-  const parts = props.parts
-  return (
-    <div>
-      <Part part={parts[0].name} exercises={parts[0].exercises} />
-      <Part part={parts[1].name} exercises={parts[1].exercises} />
-      <Part part={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const parts = props.parts
-  return (
-    <div>
-      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <Header course={course.name} />
-      {/* 一次性把三组数据传给 Content，只渲染一次 Content */}
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
