f74dd1f9e98f80402dfa161607a9cdc9fb5884cd
idontknow <1245785295@qq.com>
2026-06-29 14:23:10 +0800

    feat: initialize part1-01 project with React and Vite setup
    - Add package.json with dependencies and scripts for development
    - Create favicon.svg for the project
    - Add icons.svg containing various SVG icons
    - Implement App component with Header, Content, and Total components
    - Set up main entry point for React application
    - Configure Vite for React project
    - Create a test file in part1-02 to demonstrate array manipulation

---
+++ part1/part1-01/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+// 重构后的 Content 组件，内部渲染三个 Part
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.temp1 + props.temp2 + props.temp3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      {/* 一次性把三组数据传给 Content，只渲染一次 Content */}
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total temp1={exercises1} temp2={exercises2} temp3={exercises3} />
+    </div>
+  )
+}
+
+export default App
