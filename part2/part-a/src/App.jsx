0900fc310d7b4250eace77cd191bd7f1858cd687
idontknow <1245785295@qq.com>
2026-06-30 16:08:16 +0800

    part2

---
+++ part2/part-a/src/App.jsx
@@ -0,0 +1,17 @@
+const App = ({ note }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(
+          note =>
+            <li key={note.id}>
+              {note.content}
+            </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
