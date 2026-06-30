17c0d1ce0336d8e7381af99b4c95a2e0f6d678b8
idontknow <1245785295@qq.com>
2026-06-30 09:55:46 +0800

    异步问题

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,32 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name},you are {props.age} years old</p>
-    </div>
-  )
-}
-
+import { useState } from 'react'
 const App = () => {
-  const name = 'Peter'
-  const age = 10
-  const friend = ['xiaoming', 'zhangsan']
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    setTotal(right + left)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(right + left)
+  }
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <p>{friend[0]} {friend[1]}</p>
-      <Hello name="George" age={26 + 10} />
-      <Hello name="Daisy" age={age} />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>{total}</p>
     </div>
   )
 }
-
 export default App
