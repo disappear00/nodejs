2b7c421437325da523921329d4a731d05d23a064
idontknow <1245785295@qq.com>
2026-07-02 10:14:32 +0800

    表单提交事件

--- part2/part-a/src/App.jsx
+++ part2/part-a/src/App.jsx
@@ -4,11 +4,33 @@ import { useState } from 'react'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note ...')
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
 
   return (
     <div>
       <h1>Notes</h1>
       <Note notes={notes}></Note>
+      <form onSubmit={addNote}>
+        <input value={newNote}
+          onChange={handleNoteChange} />
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
