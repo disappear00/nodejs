a39d99197c16364af95c94c18a46d72b75b503f9
idontknow <1245785295@qq.com>
2026-07-02 13:32:49 +0800

    表单用户信息

--- part2/part-a/src/App.jsx
+++ part2/part-a/src/App.jsx
@@ -1,36 +1,77 @@
-import Note from './components/Note'
 import { useState } from 'react'
 
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note ...')
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value);
-    setNewNote(event.target.value)
-  }
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
+const App = () => {
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+      number: '13785985529',
+    },
+    {
+      name: 'wanghaoran',
+      number: '15731985529',
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchStr, setSearchStr] = useState('')
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // 去除首尾空格
+    const trimmedName = newName.trim()
+    const trimmedNumber = newNumber.trim()
+    // 禁止空名字
+    if (!trimmedName) return
+    if (!trimmedNumber) return
+    // 查找重复姓名或手机号
+    const existPerson = persons.find(p => p.name === trimmedName || p.number === trimmedNumber)
+
+    if (existPerson) {
+      alert(`${trimmedName} is already added to phonebook`)
+      return // 重复直接终止，不新增
+    }
+
+    const newNameObject = { name: trimmedName, number: trimmedNumber }
+    setPersons(persons.concat(newNameObject))
+    setNewName('')
+    setNewNumber('')
   }
+
+  // 新增：过滤逻辑
+  const filteredPersons = persons.filter(person => {
+    // 统一转小写，匹配不区分大小写
+    const lowerName = person.name.toLowerCase()
+    const lowerSearch = searchStr.toLowerCase()
+    return lowerName.includes(lowerSearch)
+  })
 
   return (
     <div>
-      <h1>Notes</h1>
-      <Note notes={notes}></Note>
-      <form onSubmit={addNote}>
-        <input value={newNote}
-          onChange={handleNoteChange} />
-        <button type='submit'>save</button>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={searchStr} onChange={e => setSearchStr(e.target.value)}></input>
+        </div>
       </form>
+      <h2>add a new</h2>
+      <div>debug {newName}</div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={e => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}></input>
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {/* 遍历过滤后的数组 filteredPersons */}
+        {filteredPersons.map(person => (
+          <li key={person.name}>{person.name} {person.number}</li>
+        ))}
+      </ul>
     </div>
   )
 }
