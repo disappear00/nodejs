c6fedfe1681427335c54537624ee626d3c160ba7
idontknow <1245785295@qq.com>
2026-07-03 13:03:46 +0800

    part2-finitely

--- part2/part-a/src/App.jsx
+++ part2/part-a/src/App.jsx
@@ -2,17 +2,30 @@ import Notes from './components/Note'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import noteService from './services/notes'
+import './index.css'
+import Footer from './components/footer'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note ...')
-
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
   useEffect(() => {
     noteService
       .getAll()
-      .then(response => {
-        setNotes(response.data)
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
   console.log('render', notes.length, 'notes');
@@ -21,9 +34,17 @@ const App = () => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
     noteService
-      .update(id, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => note.id === id ? response.data : note))
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
@@ -49,6 +70,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={errorMessage} />
       <h1>Notes</h1>
       <Notes notes={notes} toggleImportanceOf={(id) => toggleImportanceOf(id)}></Notes>
       <form onSubmit={addNote}>
@@ -56,6 +78,7 @@ const App = () => {
           onChange={handleNoteChange} />
         <button type='submit'>save</button>
       </form>
+      <Footer></Footer>
     </div>
   )
 }
